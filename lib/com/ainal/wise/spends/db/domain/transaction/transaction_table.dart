6c1200f0fec80c11cad01afa6b852116ee0ebf53
Ainal-Farhan <mohd.ainalfarhan7@gmail.com>
2022-12-05 09:34:56 +0800

    update db domain transaction table

--- lib/com/ainal/wise/spends/db/domain/transaction/transaction_table.dart
+++ lib/com/ainal/wise/spends/db/domain/transaction/transaction_table.dart
@@ -1,6 +1,7 @@
 import 'package:drift/drift.dart';
 import 'package:wise_spends/com/ainal/wise/spends/constant/domain/domain_table_constant.dart';
 import 'package:wise_spends/com/ainal/wise/spends/db/domain/base/base_entity_table.dart';
+import 'package:wise_spends/com/ainal/wise/spends/db/domain/expense/expense_table.dart';
 import 'package:wise_spends/com/ainal/wise/spends/db/domain/saving/saving_table.dart';
 
 @DataClassName(DomainTableConstant.transactionTablePrefix + "Transaction")
@@ -12,4 +13,6 @@ class TransactionTable extends BaseEntityTable {
   RealColumn get amount =>
       real().check(amount.isBiggerThan(const Constant(0)))();
   TextColumn get savingId => text().references(SavingTable, #id)();
+  BoolColumn get isExpense => boolean().withDefault(const Constant(false))();
+  TextColumn get expenseId => text().nullable().references(ExpenseTable, #id)();
 }
